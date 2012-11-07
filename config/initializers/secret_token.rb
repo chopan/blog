114ef06743dbb6a815c501583793181f7d28dbb7
Carlos Garcia <chopan@gmail.com>
2012-11-06 18:09:09 -0700

    Se agregas los archivos del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unidep::Application.config.secret_token = '730c891a5601b9e78f90df05471699cc38112860c5651789c8ac3aed91b1985e8aaacf93dfd8006b16e152d08c6bfcdb87ed20dc940cf84b1863d2f4022d4c3a'
